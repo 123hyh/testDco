5f6de84ca2298b7b6ba56f14cf7479bafd433634
123hyh <310515005@qq.com>
2020-05-20 23:48:03 +0800

    列表优化

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,74 +1,130 @@
 <template>
-  <div class="list-component" ref="list-box" >
-    <template v-for="(item, i) of tempList" >
+  <div class="list-component" ref="list-box">
+    <template v-for="(item, i) of tempList">
       <div
-        :data-serial='item'
-        :ref="i === 10 ? 
-          'list-top' : i === tempList.length - 10? 
-          'list-bottom' : undefined"   
+        :data-serial="item"
+        :ref="
+          i === 10
+            ? 'list-top'
+            : i === tempList.length - 10
+            ? 'list-bottom'
+            : undefined
+        "
         :key="item"
-      >{{item}}</div>
+      >
+        {{ item }}
+      </div>
     </template>
-    
   </div>
 </template>
 <script>
 /* eslint-disable no-debugger */
-export default {
-  props:{
-    list:{
-      type: Array,
-      default: ()=>(Array.from({length: 100000}).map((_, i) => i+1))
+
+// 初始化长列表
+function initLongList(cb = () => {}, { refStr } = {}) {
+  let loading = false;
+  let handler = null;
+  let observer = null;
+  return function() {
+    // 判断是否要处理继续往下处理逻辑
+    if (
+      this.$refs[refStr] &&
+      Object.prototype.toString.call(this.$refs[refStr][0]).slice(8, -1) ===
+        'HTMLDivElement'
+    ) {
+      // 单例
+      observer =
+        observer ||
+        new IntersectionObserver(([entries]) => {
+          if (entries.isIntersecting) {
+            if (loading) {
+              return;
+            }
+            // 取消订阅上一个
+            observer.unobserve(entries.target);
+
+            // 打开节流阀
+            handler && clearTimeout(handler);
+            loading = true;
+
+            handler = setTimeout(() => {
+              const target = this.$refs['list-box'];
+              target.scrollTop = target.scrollTop / 2;
+              if (
+                (refStr === 'list-bottom' &&
+                  this.start + this.pageSize >= this.list.length) ||
+                (refStr === 'list-top' &&
+                  this.start !== 0 &&
+                  this.start - this.pageSize <= 0)
+              ) {
+                return;
+              }
+              // 执行回调函数
+              cb.call(this, entries.target);
+
+              // 关闭节流阀
+              loading = false;
+
+              this.$nextTick(() => {
+                this.handlerScrollTopEvent();
+                this.handlerBottomVisible();
+              });
+            }, 70);
+          }
+        });
+      observer.observe(this.$refs[refStr][0]);
+    } else {
+      console.warn(
+        `数据量 < props.pageSize \${${this.pageSize}} ${this.pageSize}, 不进行长列表优化~`
+      );
     }
-  },
-  mounted(){
-    this.handlerBottomVisible()
-  },
-  methods:{
-    // 底部可见时事件
-    handlerBottomVisible:(() => {
-      let loading = false;
-      let handler = null;
-      return function() {
-      const ob = new IntersectionObserver(([entries])=>{
-        if(entries.isIntersecting){
-
-          if ( loading ) return;
-
-          // 打开节流阀
-          handler && clearTimeout(handler)
-          loading = true
-
-          handler = setTimeout(() => {
-
-            const target = this.$refs['list-box']
-            target.scrollTop = target.scrollTop / 2
-            this.tempList = this.list.slice(this.start+=100 - 50, this.end+=100 - 50)
-
-            // 关闭节流阀
-            loading = false;
-
-            // 取消订阅
-            ob.unobserve(entries.target)
-            this.$nextTick(this.handlerBottomVisible)
-          },150)
-        }
-      })
-      ob.observe(this.$refs['list-bottom'][0])
-    }
-    })()
-  },
-  data(){ 
-    return  {
-    start: 0,
-    end: 100,
-    tempList: this.list.slice(0, 100)
-  }
-  }
+  };
 }
+export default {
+  props: {
+    /* 容器中的条目数 */
+    pageSize: {
+      type: Number,
+      default: 50,
+    },
+    /* 源列表数据 */
+    list: {
+      type: Array,
+      default: () => Array.from({ length: 500 }).map((_, i) => i + 1),
+    },
+  },
+  mounted() {
+    this.handlerBottomVisible();
+  },
+  methods: {
+    // 顶部可见时事件
+    handlerScrollTopEvent: initLongList(
+      function() {
+        console.log(1);
+      },
+      { refStr: 'list-top' }
+    ),
+    handlerBottomVisible: initLongList(
+      function() {
+        const start = (this.start += this.pageSize);
+        const end = (this.end += this.pageSize);
+        const calc = this.pageSize / 2;
+        this.tempList = this.list.slice(start - calc, end - calc);
+      },
+      { refStr: 'list-bottom' }
+    ),
+  },
+  data() {
+    return {
+      start: 0,
+      end: 100,
+      tempList: this.list.slice(0, 100),
+    };
+  },
+};
 </script>
-<style >
-.list-component{
+<style>
+.list-component {
   background-color: #f0f0f0;
   margin: 20px;
   height: 300px;
